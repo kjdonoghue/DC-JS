98c31ef255607b184208408f6467e2612df5a899
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-09-30 17:30:54 -0400

    wed menu assignement js

---
+++ app.js
@@ -0,0 +1,84 @@
+let menuList = document.getElementById("menuList")
+
+let menuItem = dishes.map(function(item) {
+    return `<li class="menuItem">
+    <div><img src=${item.imageURL}></img></div>
+    <div><b> ${item.title} </b></div>
+    <div><label> ${item.description} </label></div>
+    <div><label> ${item.price}</label></div>
+        `
+})
+    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+
+
+function displayAll(){
+    menuList.innerHTML = ""
+    let menuItem = dishes.map(function(item) {
+        return `<li class="menuItem">
+        <div><img src=${item.imageURL}></img></div>
+        <div><b> ${item.title} </b></div>
+        <div><label> ${item.description} </label></div>
+        <div><label> ${item.price}</label></div>
+         `
+})
+    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+}
+
+function displayMenuItems(list){
+    menuList.innerHTML = ""
+    let menuItem = list.map(function(item) {
+        return `<li class="menuItem">
+        <div><img src=${item.imageURL}></img></div>
+        <div><b> ${item.title} </b></div>
+        <div><label> ${item.description} </label></div>
+        <div><label> ${item.price}</label></div>
+         `
+})
+    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+}
+
+
+function displayStarters(){
+    menuList.innerHTML = ""
+    let menuItem = dishes.map(function(item) {
+        if (item.course == "Starters")
+            return `<li class="menuItem">
+            <div><img src=${item.imageURL}></img></div>
+            <div><b> ${item.title} </b></div>
+            <div><label> ${item.description} </label></div>
+            <div><label> ${item.price}</label></div>
+        `
+    })
+    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+}
+
+
+function displayEntrees(){
+    menuList.innerHTML = ""
+    let menuItem = dishes.map(function(item) {
+        if (item.course == "Entrees")
+            return `<li class="menuItem">
+            <div><img src=${item.imageURL}></img></div>
+            <div><b> ${item.title} </b></div>
+            <div><label> ${item.description} </label></div>
+            <div><label> ${item.price}</label></div>
+        `
+    })    
+    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+}
+
+function displayDesserts(){
+    menuList.innerHTML = ""
+    let menuItem = dishes.map(function(item) {
+        if (item.course == "Desserts")
+            return `<li class="menuItem">
+            <div><img src=${item.imageURL}></img></div>
+            <div><b> ${item.title} </b></div>
+            <div><label> ${item.description} </label></div>
+            <div><label> ${item.price}</label></div>
+        `
+    })
+    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+
+}
+
