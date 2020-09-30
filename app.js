7ba008d69f1835abf0a6426e46430ba1a495e5e1
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-09-30 18:53:58 -0400

    menu assignment - refactored to run display all upon opening and delete commented out code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let menuList = document.getElementById("menuList")
+
 let starters = dishes.filter(function(items){
     return items.course == "Starters"
     })
@@ -10,23 +11,11 @@ let entrees = dishes.filter(function(items){
 let desserts = dishes.filter(function(items){
     return items.course == "Desserts"
     })
-
-let menuItem = dishes.map(function(item) {
-    return `<li class="menuItem">
-    <div><img src=${item.imageURL}></img></div>
-    <div><b> ${item.title} </b></div>
-    <div><label> ${item.description} </label></div>
-    <div><label> ${item.price}</label></div>
-        `
-})
-    menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
-
-
         
-    function displayMenu(list){
-        menuList.innerHTML = ""
-        let menuItem = list.map(function(item) {
-        return `<li class="menuItem">
+function displayMenu(list){
+    menuList.innerHTML = ""
+    let menuItem = list.map(function(item) {
+    return `<li class="menuItem">
         <div><img src=${item.imageURL}></img></div>
         <div><b> ${item.title} </b></div>
         <div><label> ${item.description} </label></div>
@@ -34,80 +23,6 @@ let menuItem = dishes.map(function(item) {
         `
         })
     
-        menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
+     menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
     
     }
-
-
-
-// function displayAll(){
-//     menuList.innerHTML = ""
-//     let menuItem = dishes.map(function(item) {
-//         return `<li class="menuItem">
-//         <div><img src=${item.imageURL}></img></div>
-//         <div><b> ${item.title} </b></div>
-//         <div><label> ${item.description} </label></div>
-//         <div><label> ${item.price}</label></div>
-//          `
-// })
-//     menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
-// }
-
-// function displayMenuItems(list){
-//     menuList.innerHTML = ""
-//     let menuItem = list.map(function(item) {
-//         return `<li class="menuItem">
-//         <div><img src=${item.imageURL}></img></div>
-//         <div><b> ${item.title} </b></div>
-//         <div><label> ${item.description} </label></div>
-//         <div><label> ${item.price}</label></div>
-//          `
-// })
-//     menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
-// }
-
-
-// function displayStarters(){
-//     menuList.innerHTML = ""
-//     let menuItem = dishes.map(function(item) {
-//         if (item.course == "Starters")
-//             return `<li class="menuItem">
-//             <div><img src=${item.imageURL}></img></div>
-//             <div><b> ${item.title} </b></div>
-//             <div><label> ${item.description} </label></div>
-//             <div><label> ${item.price}</label></div>
-//         `
-//     })
-//     menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
-// }
-
-
-// function displayEntrees(){
-//     menuList.innerHTML = ""
-//     let menuItem = dishes.map(function(item) {
-//         if (item.course == "Entrees")
-//             return `<li class="menuItem">
-//             <div><img src=${item.imageURL}></img></div>
-//             <div><b> ${item.title} </b></div>
-//             <div><label> ${item.description} </label></div>
-//             <div><label> ${item.price}</label></div>
-//         `
-//     })    
-//     menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
-// }
-
-// function displayDesserts(){
-//     menuList.innerHTML = ""
-//     let menuItem = dishes.map(function(item) {
-//         if (item.course == "Desserts")
-//             return `<li class="menuItem">
-//             <div><img src=${item.imageURL}></img></div>
-//             <div><b> ${item.title} </b></div>
-//             <div><label> ${item.description} </label></div>
-//             <div><label> ${item.price}</label></div>
-//         `
-//     })
-//     menuList.insertAdjacentHTML("beforeend", menuItem.join(" "))
-
-// }
-
